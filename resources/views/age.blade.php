1b54b2b304a410b3c84d3326a6b20642f7f8bca5
Okyop <pcombes80@gmail.com>
2021-02-16 11:54:16 +0100

    final commit exo1

---
+++ resources/views/age.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="{{route('wel')}}">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="{{route('wel')}}">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{route('nom')}}">nom</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{route('prenom')}}">prenom</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{route('age')}}">age</a>
+              </li>
+          </ul>
+        </div>
+      </nav>
+    <h1>old but scratch</h1>
+    <script src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('wel');
+
+Route::get('/nom', function () {
+    return view('nom');
+})->name('nom');
+
+Route::get('/prenom', function () {
+    return view('prenom');
+})->name('prenom');
+
+Route::get('/age', function () {
+    return view('age');
+})->name('age');
